d60eed9dd50e858c2f1e851752c0e45a51c02a28
bagav1 <bragaru@hotmail.es>
2022-06-01 18:50:01 -0500

    Nav

---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+export default function nav() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#!">Text Consume API</Navbar.Brand>
+      </Navbar>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,24 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Nav from './components/nav';
 import List from './components/List';
 import TableCreated from './components/formCreate';
 import { Row, Col, Container } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Container>
-  <Row>
-    <Col lg={6}>
-      <h3 style={{textAlign: 'center'}}>Lista</h3>
-      <List />
-    </Col>
-    <Col lg={{ span: 5, offset: 1 }}>
-      <h3 style={{textAlign: 'center'}}>Crear nuevo registro</h3>
-      <TableCreated />
-    </Col>
-  </Row>
-  </Container>
+  <React.StrictMode>
+    <Nav />
+    <Container>
+      <Row>
+        <Col lg={6}>
+          <h3 style={{ textAlign: 'center' }}>Lista</h3>
+          <List />
+        </Col>
+        <Col lg={{ span: 5, offset: 1 }}>
+          <h3 style={{ textAlign: 'center' }}>Crear nuevo registro</h3>
+          <TableCreated />
+        </Col>
+      </Row>
+    </Container>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
